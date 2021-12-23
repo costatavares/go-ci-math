33dc93a62fe2a5ce32d0598209f2ca630430af70
costatavares <costatavares222@gmail.com>
2021-12-23 11:20:47 -0300

    test: Ajusted test to assert value

--- math_test.go
+++ math_test.go
@@ -9,7 +9,7 @@ func TestSoma(t *testing.T) {
 
 	total := Soma(15, 15)
 
-	esperado := 35
+	esperado := 30
 
 	if total != esperado {
 		t.Errorf("Resultado da soma é invalido: Resultado %d.  Esperado %d.", total, esperado)
